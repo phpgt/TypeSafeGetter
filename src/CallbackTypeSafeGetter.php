86d1197fa99c153254b53f92f287c734faa085c5
Greg Bowler <greg.bowler@g105b.com>
2022-07-08 17:33:45 +0100

    feature: force non-null callback

--- src/CallbackTypeSafeGetter.php
+++ src/CallbackTypeSafeGetter.php
@@ -4,10 +4,10 @@ namespace Gt\TypeSafeGetter;
 use DateTimeInterface;
 
 interface CallbackTypeSafeGetter {
-	public function get(string $name, ?callable $callback = null):mixed;
-	public function getString(string $name, ?callable $callback = null):?string;
-	public function getInt(string $name, ?callable $callback = null):?int;
-	public function getFloat(string $name, ?callable $callback = null):?float;
-	public function getBool(string $name, ?callable $callback = null):?bool;
-	public function getDateTime(string $name, ?callable $callback = null):?DateTimeInterface;
+	public function get(string $name, callable $callback):mixed;
+	public function getString(string $name, callable $callback):?string;
+	public function getInt(string $name, callable $callback):?int;
+	public function getFloat(string $name, callable $callback):?float;
+	public function getBool(string $name, callable $callback):?bool;
+	public function getDateTime(string $name, callable $callback):?DateTimeInterface;
 }
